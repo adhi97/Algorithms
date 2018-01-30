655340054dfea75fbc8608fe2d473e25b92ed1fd
Adheesh <adhi97@gmail.com>
2018-01-30 17:11:10 -0500

    making changes to the algorithm for array based coding q's

--- Coding Challenges/find_min_max.py
+++ Coding Challenges/find_min_max.py
@@ -11,18 +11,28 @@ class Solution:
 
     @staticmethod
     def find_contiguous_sum(list_: List) -> int:
-        max_int = -999999
-        max_till_here = 0
-        for i in range(len(list_)):
-            max_till_here = max_till_here + list_[i]
-            if(max_int < max_till_here):
-                max_int = max_till_here
+        max_till_now, max_int = list_[0], list_[0]
+        for item in list_:
+            max_till_now = max_till_now + item if max_till_now + item > item else item
+            max_int = max_till_now if max_till_now > max_int else max_int
         return max_int
+
+    @staticmethod
+    def remove_duplicates(list_: List) -> List[int]:
+        buff = {}
+        for i in range(len(list_)):
+            if list_[i] in buff:
+                list_.remove(list_[i])
+            else:
+                buff[list_[i]] = i
+        return list_
 
 
 if __name__ == "__main__":
-    arr = [2, -3, 4, -5, 3]
+    arr = [2, -3, 4, -5, 3, 4]
     min_val = Solution.find_min(arr)
     max_sum = Solution.find_contiguous_sum(arr)
+    list_no_duplicates = Solution.remove_duplicates(arr)
     print(min_val)
     print(max_sum)
+    print(list_no_duplicates)
